fbbeda2244566d1e5b98b9f30edc496f678fa5f6
murtaza192168 <murtazacloudwork1252@gmail.com>
2024-04-06 10:19:36 +0530

    Add a search bar in navbar

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import React from 'react'
 import logo from '../imgs/logo.png'
 
@@ -5,8 +6,15 @@ const Navbar = () => {
   return (
     <nav className='navbar'>
         {/*Logo*/}
-        <img src={logo} className='flex-none w-10' alt="" />
+        <Link to='/' className='flex-none w-10'>
+        <img src={logo} className='w-full' alt="" /></Link>
         {/* search bar */}
+        <div className='absolute bg-white w-full left-0 mt-0.5 top-full  border-b border-grey py-5 px-[5vw]'>
+            <input type="text"
+                   placeholder='Search'
+                   className='w-full md:w-auto bg-grey p-4 pl-6 pr-[12%]
+                    md: pr-6 rounded-full placeholder:text-dark-grey' />
+        </div>
         {/* sign in sign up button */}
         {/* if user has logged in, will show the user image on right corner */}
 
